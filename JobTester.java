7d6d48cc10a18fc286f90de0053b68304d40c196
ShrikarRM <you@example.com>
2020-08-19 17:56:08 +0530

    collection example

---
+++ JobTester.java
@@ -0,0 +1,12 @@
+package com.account.collectionfw;
+
+public class JobTester {
+
+	public static void main(String[] args) {
+		Job j = new Job();
+		j.salary = 20000;
+		j.name = "MS";
+		System.out.println(j.toString());
+	}
+
+}
