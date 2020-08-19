70bc6112f89e48c503e708bf1d6c83896685a72d
ShrikarRM <you@example.com>
2020-08-19 17:52:18 +0530

    collection example

---
+++ Job.java
@@ -0,0 +1,11 @@
+package com.account.collectionfw;
+
+public class Job {
+	double salary;
+	String name;
+
+	@Override
+	public String toString() {
+		return "[Salary=" + salary + ", Company Name=" + name + "]";
+	}
+}
